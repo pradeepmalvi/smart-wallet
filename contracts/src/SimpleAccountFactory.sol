f70f0b800819d1d8b3849215c62f46106618e075
pradeepmalvi <p.malviyaaa@gmail.com>
2024-12-06 16:05:20 +0530

    version 02 tested

--- contracts/src/SimpleAccountFactory.sol
+++ contracts/src/SimpleAccountFactory.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.12;
 
 import "openzeppelin-contracts/contracts/utils/Create2.sol";
 import "openzeppelin-contracts/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "src/SimpleAccount.sol";
 
 struct User {
@@ -11,86 +12,81 @@ struct User {
     address account;
 }
 
-/**
- * A sample factory contract for SimpleAccount
- * A UserOperations "initCode" holds the address of the factory, and a method call (to createAccount, in this sample factory).
- * The factory's createAccount returns the target account address even if it is already installed.
- * This way, the entryPoint.getSenderAddress() can be called either before or after the account is created.
- */
-contract SimpleAccountFactory {
-    SimpleAccount public immutable accountImplem;
+contract SimpleAccountFactory is ReentrancyGuard {
+    SimpleAccount public accountImplem;
     IEntryPoint public immutable entryPoint;
-    bytes32 public constant SALT = keccak256("hocuspocusxyz");
+    address public owner;
 
-    mapping(uint256 id => User user) public users;
+    mapping(uint256 => User) public users;
+
+    event UserSaved(uint256 indexed id, bytes32[2] publicKey, address account);
+    event AccountCreated(address indexed account, bytes32[2] publicKey);
 
     constructor(IEntryPoint _entryPoint) {
+        owner = msg.sender;
         entryPoint = _entryPoint;
         accountImplem = new SimpleAccount(_entryPoint);
     }
 
-    function saveUser(uint256 id, bytes32[2] memory publicKey) external {
-        users[id] = User(id, publicKey, this.getAddress(publicKey));
+    function saveUser(uint256 id, bytes32[2] memory publicKey) external nonReentrant {
+        require(users[id].id == 0, "User ID already exists");
+        address calculatedAddress = this.getAddress(publicKey);
+        users[id] = User(id, publicKey, calculatedAddress);
+        emit UserSaved(id, publicKey, calculatedAddress);
     }
 
     function getUser(uint256 id) external view returns (User memory) {
         return users[id];
     }
 
-    /**
-     * Create an account, and return its address.
-     * Returns the address even if the account is already deployed.
-     * Note that during UserOperation execution, this method is called only if the account is not deployed.
-     * This method returns an existing account address so that entryPoint.getSenderAddress() would work even after account creation.
-     */
     function createAccount(
         bytes32[2] memory publicKey
-    ) external payable returns (SimpleAccount) {
+    ) external payable nonReentrant returns (SimpleAccount) {
         address addr = getAddress(publicKey);
 
-        // Prefund the account with msg.value
         if (msg.value > 0) {
             entryPoint.depositTo{value: msg.value}(addr);
         }
 
-        // Otherwise, no-op if the account is already deployed
-        uint codeSize = addr.code.length;
-        if (codeSize > 0) {
+        if (addr.code.length > 0) {
             return SimpleAccount(payable(addr));
         }
 
-        return
-            SimpleAccount(
-                payable(
-                    new ERC1967Proxy{salt: SALT}(
-                        address(accountImplem),
-                        abi.encodeCall(SimpleAccount.initialize, (publicKey))
-                    )
-                )
-            );
+        address proxyAddress = address(
+            new ERC1967Proxy{salt: keccak256(abi.encodePacked(publicKey))}(
+                address(accountImplem),
+                abi.encodeCall(SimpleAccount.initialize, (publicKey))
+            )
+        );
+
+        emit AccountCreated(proxyAddress, publicKey);
+        return SimpleAccount(payable(proxyAddress));
     }
 
-    /**
-     * Calculate the counterfactual address of this account as it would be returned by createAccount()
-     */
     function getAddress(
         bytes32[2] memory publicKey
     ) public view returns (address) {
         return
             Create2.computeAddress(
-                SALT,
+                keccak256(abi.encodePacked(publicKey)),
                 keccak256(
                     abi.encodePacked(
                         type(ERC1967Proxy).creationCode,
                         abi.encode(
                             address(accountImplem),
-                            abi.encodeCall(
-                                SimpleAccount.initialize,
-                                (publicKey)
-                            )
+                            abi.encodeCall(SimpleAccount.initialize, (publicKey))
                         )
                     )
                 )
             );
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "only Owner");
+        _;
+    }
+    
+    function setImplementation(SimpleAccount newImplementation) external onlyOwner {
+        accountImplem = newImplementation;
     }
 }
